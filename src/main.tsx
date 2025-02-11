55c721edb2e8a632083e07c6d8be1d380170a7b6
Marcos Aresqueta <marcos.aresqueta@gmail.com>
2025-02-11 17:29:43 +0100

    Set AppRoutes and create an icons factory.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/infrastructure/react-query/queryClient';
 import { App } from '@/App';
+import '@/presentation/styles/variables.css';
+import '@/presentation/styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </QueryClientProvider>,
 );
