bb396a3411fff50b3086a1e1d208a04e9a2b9f53
Marcos Aresqueta <marcos.aresqueta@gmail.com>
2025-02-11 16:59:59 +0100

    Add image assets, and  a custom hook to manage language for english locale messages.

---
+++ src/presentation/locales/en.ts
@@ -0,0 +1,9 @@
+export default {
+  NOT_FOUND: 'Page not found.',
+  NETWORK_ISSUES: 'Network issues.',
+  SERVICE_UNAVAILABLE: 'Resource unavailable.',
+  RESULTS: 'RESULTS',
+  NO: 'NO',
+  FAVOURITES: 'FAVORITES',
+  COMICS: 'COMICS',
+};
